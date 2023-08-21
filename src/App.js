b9df73401f4c744c6e285ed30cf165ba8315d3d4
veeramadhan <95845583+veeramadhan@users.noreply.github.com>
2023-08-21 23:27:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import Tables from "../src/components/Tables/Tables"
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <center>
+      Fruits Benifits
+    </center>
+    <Tables/>
     </div>
   );
 }
---
+++ src/components/Tables/Tables.js
@@ -0,0 +1,56 @@
+import Table from 'react-bootstrap/Table';
+import { useState,useEffect} from 'react';
+import data from "./data.json"
+
+function DarkExample() {
+
+    const[name,setName]=useState([])
+    const[search,setSearch]=useState(name)
+    
+    const url="../fruit/public/assets/pictures"
+    
+    const handleChange=(event)=>{
+        setSearch(name.filter(val=>val.names.toLowerCase().includes(event.target.value)))
+    }
+
+    useEffect(()=>{
+        setName(data)
+        setSearch(data)
+    },[])
+
+  return (
+    <>
+    <div>
+         <label>Data:</label>
+   <input type="text" placeholder="Enter a input data" onChange={handleChange}/>
+    </div>
+    <Table striped bordered hover variant="dark">
+
+  
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Fruits Name</th>
+          <th>Benifits</th>
+          <th>Image</th>
+        </tr>
+      </thead>
+      <tbody>
+        {search.map((value,index)=>(
+          <tr key={index}>
+          <td>{value.id}</td>
+          <td>{value.names}</td>
+          <td>{value.benifit}</td>
+          <td> <img src={value.image} alt="" width="200" height="150" /></td>
+        </tr>
+        ))}
+        
+       
+       
+      </tbody>
+    </Table>
+    </>
+  );
+}
+
+export default DarkExample;
